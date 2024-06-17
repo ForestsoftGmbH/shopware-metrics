2e431b38ed40a21e14076efe8654809d8ce8d16f
Sebastian <foerster@forestsoft.de>
2024-06-17 16:33:18 +0000

    fix: sql: Scan error on column index 1, name "name": converting NULL to string is unsupported

--- shopware/main.go
+++ shopware/main.go
@@ -11,7 +11,7 @@ type SalesChannel struct {
 }
 
 func GetSalesChannels(db *sql.DB) []SalesChannel {
-	res, err := db.Query("SELECT sales_channel.id, sales_channel_translation.name FROM sales_channel INNER JOIN sales_channel_translation ON sales_channel.id = sales_channel_translation.sales_channel_id")
+	res, err := db.Query("SELECT sales_channel.id, sales_channel_translation.name FROM sales_channel INNER JOIN sales_channel_translation ON sales_channel.id = sales_channel_translation.sales_channel_id WHERE sales_channel.active = 1 AND sales_channel_translation.name IS NOT NULL")
 	if err != nil {
 		log.Fatal(err)
 	}
